ed187ae7dcbe7282d7597db37d6402b5de7b6781
MARTIN Tom <tmartin@cg86.fr>
2023-04-14 16:20:03 +0200

    ajout de la gestion des erreurs

--- geocodage_ass_mat.py
+++ geocodage_ass_mat.py
@@ -1,14 +1,17 @@
 #imports
 import requests
-import csv
+
 
 def push(fichier):
     """Envoie une requete à la Ban et crée un fichier avec les valeurs de retour
     """    
     #le fichier csv à push
-    csv_donnes = open(fichier)
+    try:
+        csv_donnes = open(fichier)
+    except:
+        exit("Impossible d'ouvrir le fichier")
     #url API
-    url = 'https://api-adresse.data.gouv.fr/search/csv/'
+    url = 'https://api-adresse.data.gouv.fr/kjnlknlnsearch/csv/dd'
     #paramètres pour la requete
     params = {
         'columns': ['ADRESSE'],
@@ -17,12 +20,13 @@ def push(fichier):
     }   
     #execution de la requete avec les params du dessus et le fichier csv push
     test = requests.post(url, data=params, files={"data": csv_donnes}, timeout=5000)
+    #fermer le fichier
+    csv_donnes.close()
     #on verifie si on obtient un retour positif de la requete
     if test.status_code == 200:
         print(test.text)
-
     else:
-        print("Erreur lors de la requête.")
+        exit("Erreur API BAN : "+test.reason)
 
 def main():
     """Main du script
